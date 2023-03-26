98972cb6d8c6e2532568017617e442ef6ecd2c91
Noah Larsen <noahlarsen24@gmail.com>
2023-03-26 10:24:05 -0500

    changed file structure

---
+++ src/components/tracker/Tracker.js
@@ -0,0 +1,32 @@
+import Overview from "./Overview";
+import Progress from "./Progress";
+import Progression from "./Progression";
+import CurrentBmi from "./CurrentBmi";
+import EnterWeightBtn from "../EnterWeightBtn";
+import { useContext } from "react";
+import { TrackerContext } from "../../App";
+
+function Tracker() {
+  const { openFormHandler } = useContext(TrackerContext);
+  return (
+    <main className="tracker">
+      <Overview />
+      <Progress />
+      <section className="current-stats">
+        <h2>current statistics</h2>
+      </section>
+      <Progression />
+      <CurrentBmi />
+      <section className="enter-weight">
+        <EnterWeightBtn
+          onClick={openFormHandler}
+          divClassName={"enter-weight-btn"}
+          btnClassName={"weight-btn"}
+          input={"enter statistics"}
+        />
+      </section>
+    </main>
+  );
+}
+
+export default Tracker;

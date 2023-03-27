1f4de9a1dde20acca1a9e3091fdccc66821c56ed
Noah Larsen <noahlarsen24@gmail.com>
2023-03-27 11:45:23 -0500

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 function Navbar() {
   return (
     <nav>
+      <button className="hamburger-btn">
+        <FontAwesomeIcon icon={faBars} />
+      </button>
       <ul>
         <li>Current Data</li>
         <li>History</li>

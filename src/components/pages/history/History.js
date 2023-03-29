2cd0839efcd3c4302408418160a5d1e9498e3698
Noah Larsen <noahlarsen24@gmail.com>
2023-03-29 11:28:05 -0500

    updated scss file structure

--- src/components/pages/history/History.js
+++ src/components/pages/history/History.js
@@ -1,5 +1,33 @@
 function History() {
-  return <div>hello world</div>;
+  return (
+    <>
+      <header className="heading">
+        <div className="title">
+          <h2>history</h2>
+        </div>
+        <div className="name">
+          <h3>firstName</h3>
+        </div>
+      </header>
+      <section className="history">
+        <div className="history-container">
+          <div className="input-history">
+            <p className="input">57kg</p>
+            <p className="difference">-2kg</p>
+            <div className="day-date">
+              <p>March</p>
+              <p>17</p>
+              <p>2023</p>
+            </div>
+            <div className="date-time">
+              <p>Friday</p>
+              <p>1:10 AM</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default History;

43387b470582db225e78137aa4569158f09ed908
Noah Larsen <noahlarsen24@gmail.com>
2023-04-01 11:09:08 -0500

    fixed responsiveness of profile page

--- src/components/pages/profile/Profile.js
+++ src/components/pages/profile/Profile.js
@@ -31,110 +31,114 @@ function Profile() {
       </header>
       <section className="input-form">
         <div className="input-form-container">
-          <h3>profile details</h3>
-          <form
-            className="form profile-details"
-            onSubmit={(e) => {
-              e.preventDefault();
-            }}
-          >
-            <div className="form-container">
-              <div className="input-container">
-                <FormInput
-                  htmlFor={"first-name"}
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                  type={"text"}
-                  name={"first-name"}
-                  id={"first-name"}
-                >
-                  first name:
-                </FormInput>
-                <FormInput
-                  htmlFor={"last-name"}
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                  type={"text"}
-                  name={"last-name"}
-                  id={"last-name"}
-                >
-                  last name:
-                </FormInput>
-                <FormInput
-                  htmlFor={"birthdate"}
-                  value={birthDate}
-                  onChange={(e) => setBirthDate(e.target.value)}
-                  type={"date"}
-                  name={"birthdate"}
-                  id={"birthdate"}
-                >
-                  date of birth:
-                </FormInput>
-                <FormInput
-                  htmlFor={"meters"}
-                  value={meters}
-                  onChange={(e) => setMeters(e.target.value)}
-                  type={"text"}
-                  name={"meters"}
-                  id={"meters"}
-                >
-                  height (centimeters):
-                </FormInput>
+          <div className="profile-details-container placeholderClass">
+            <h3>profile details</h3>
+            <form
+              className="form profile-details"
+              onSubmit={(e) => {
+                e.preventDefault();
+              }}
+            >
+              <div className="form-container">
+                <div className="input-container">
+                  <FormInput
+                    htmlFor={"first-name"}
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    type={"text"}
+                    name={"first-name"}
+                    id={"first-name"}
+                  >
+                    first name:
+                  </FormInput>
+                  <FormInput
+                    htmlFor={"last-name"}
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    type={"text"}
+                    name={"last-name"}
+                    id={"last-name"}
+                  >
+                    last name:
+                  </FormInput>
+                  <FormInput
+                    htmlFor={"birthdate"}
+                    value={birthDate}
+                    onChange={(e) => setBirthDate(e.target.value)}
+                    type={"date"}
+                    name={"birthdate"}
+                    id={"birthdate"}
+                  >
+                    date of birth:
+                  </FormInput>
+                  <FormInput
+                    htmlFor={"meters"}
+                    value={meters}
+                    onChange={(e) => setMeters(e.target.value)}
+                    type={"text"}
+                    name={"meters"}
+                    id={"meters"}
+                  >
+                    height (centimeters):
+                  </FormInput>
+                </div>
               </div>
-            </div>
-          </form>
-          <h3>weight objectives</h3>
-          <form
-            className="form weight-objectives"
-            onSubmit={(e) => {
-              e.preventDefault();
-            }}
-          >
-            <div className="form-container">
-              <div className="input-container">
-                <FormInput
-                  htmlFor={"starting-weight"}
-                  value={startingWeight}
-                  onChange={(e) => setStartingWeight(e.target.value)}
-                  type={"number"}
-                  name={"starting-weight"}
-                  id={"starting-weight"}
-                >
-                  starting weight (kilograms):
-                </FormInput>
-                <FormInput
-                  htmlFor={"start-goal-date"}
-                  value={startingDate}
-                  onChange={(e) => setStartingDate(e.target.value)}
-                  type={"date"}
-                  name={"start-goal-date"}
-                  id={"start-goal-date"}
-                >
-                  starting date:
-                </FormInput>
-                <FormInput
-                  htmlFor={"start-goal-weight"}
-                  value={goalDate}
-                  onChange={(e) => setGoalDate(e.target.value)}
-                  type={"date"}
-                  name={"start-goal-weight"}
-                  id={"start-goal-weight"}
-                >
-                  goal date:
-                </FormInput>
-                <FormInput
-                  htmlFor={"end-goal-weight"}
-                  value={goalWeight}
-                  onChange={(e) => setGoalWeight(e.target.value)}
-                  type={"number"}
-                  name={"end-goal-weight"}
-                  id={"end-goal-weight"}
-                >
-                  goal weight (kilograms):
-                </FormInput>
+            </form>
+          </div>
+          <div className="weight-objective-container placeholderClass">
+            <h3>weight objectives</h3>
+            <form
+              className="form weight-objectives"
+              onSubmit={(e) => {
+                e.preventDefault();
+              }}
+            >
+              <div className="form-container">
+                <div className="input-container">
+                  <FormInput
+                    htmlFor={"starting-weight"}
+                    value={startingWeight}
+                    onChange={(e) => setStartingWeight(e.target.value)}
+                    type={"number"}
+                    name={"starting-weight"}
+                    id={"starting-weight"}
+                  >
+                    starting weight (kilograms):
+                  </FormInput>
+                  <FormInput
+                    htmlFor={"start-goal-date"}
+                    value={startingDate}
+                    onChange={(e) => setStartingDate(e.target.value)}
+                    type={"date"}
+                    name={"start-goal-date"}
+                    id={"start-goal-date"}
+                  >
+                    starting date:
+                  </FormInput>
+                  <FormInput
+                    htmlFor={"start-goal-weight"}
+                    value={goalDate}
+                    onChange={(e) => setGoalDate(e.target.value)}
+                    type={"date"}
+                    name={"start-goal-weight"}
+                    id={"start-goal-weight"}
+                  >
+                    goal date:
+                  </FormInput>
+                  <FormInput
+                    htmlFor={"end-goal-weight"}
+                    value={goalWeight}
+                    onChange={(e) => setGoalWeight(e.target.value)}
+                    type={"number"}
+                    name={"end-goal-weight"}
+                    id={"end-goal-weight"}
+                  >
+                    goal weight (kilograms):
+                  </FormInput>
+                </div>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </section>
     </>

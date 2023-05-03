de1a6bd7968a082fa7de483f0ff93b2de0f4e4ad
Noah Larsen <noahlarsen24@gmail.com>
2023-05-03 18:53:13 -0500

    removed comment

--- src/firebase.js
+++ src/firebase.js
@@ -14,18 +14,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth();
-
-// import firebase from "firebase/app";
-// import "firebase/auth";
-
-// const app = firebase.initializeApp({
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSENGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_ID,
-// });
-
-// export const auth = app.auth();
-// export default app;

08291d4bb6f6b6d20284c16caa063ee9d37af940
Noah Larsen <noahlarsen24@gmail.com>
2023-06-11 18:54:20 -0500

    removed unused data

--- src/formSource.js
+++ src/formSource.js
@@ -1,53 +1,3 @@
-export const registerInputs = [
-  {
-    htmlFor: "first-name",
-    type: "text",
-    id: "first-name",
-    label: "first name",
-  },
-  {
-    htmlFor: "last-name",
-    type: "text",
-    id: "last-name",
-    label: "last name",
-  },
-  {
-    htmlFor: "birthdate",
-    type: "date",
-    id: "birthdate",
-    label: "date of birth:",
-  },
-  {
-    htmlFor: "height",
-    type: "text",
-    id: "height",
-    label: "height (centimeters):",
-  },
-  {
-    htmlFor: "starting-weight",
-    type: "number",
-    id: "starting-weight",
-    label: "starting weight (kilograms):",
-  },
-  {
-    htmlFor: "start-goal-date",
-    type: "date",
-    id: "start-goal-date",
-    label: "starting date:",
-  },
-  {
-    htmlFor: "goal-date",
-    type: "date",
-    id: "goal-date",
-    label: "goal date:",
-  },
-  {
-    htmlFor: "end-goal-weight",
-    type: "number",
-    id: "end-goal-weight",
-    label: "goal weight (kilograms):",
-  },
-];
 export const profileInputs = [
   {
     htmlFor: "firstName",
@@ -86,6 +36,12 @@ export const weightInputs = [
     type: "date",
     id: "startDate",
     label: "starting date:",
+  },
+  {
+    htmlFor: "currentWeight",
+    type: "number",
+    id: "currentWeight",
+    label: "current weight:",
   },
   {
     htmlFor: "goalDate",
